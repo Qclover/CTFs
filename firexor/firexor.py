a1161a9d2e813e6eb3a73ed1245d2f9a285026af
nian-hua <41846038+nian-hua@users.noreply.github.com>
2018-07-31 23:06:24 +0800

    Create firexor.py

---
+++ firexor/firexor.py
@@ -0,0 +1,24 @@
+#打开密钥文件，读取密钥
+
+f = open('keyt.txt','r+')
+
+keyt = f.read()
+
+print(keyt)
+
+f.close()
+
+#打开密文文件，读取密文
+f = open('ciphertext.txt','r+')
+
+ciphertext = f.read()
+
+print(ciphertext)
+
+f.close()
+
+#对密文和密钥进行按位异或
+for i in range(len(ciphertext)):
+
+    print(chr(ord(ciphertext[i])^ord(plaintext[i])),end="")
+
