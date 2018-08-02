f285cd8b1a67c9dabb472cbbf7ea10881c1c1b36
nian-hua <41846038+nian-hua@users.noreply.github.com>
2018-08-02 19:35:39 +0800

    Create envirginia.py

---
+++ virginia/envirginia.py
@@ -0,0 +1,48 @@
+
+#字母表/明文/密钥
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+plaintext = ""
+
+keyt = ""
+
+def Scanf():
+
+    global plaintext,keyt
+
+    plaintext = input("请输入明文(全大写):")
+
+    keyt = input("请输入密钥:")
+
+
+def AlphaMove(alpha,drift):
+
+
+    return(alphabet[(alphabet.find(alpha)+drift)%26])
+
+
+
+def EnCryption():
+
+    for i in range(len(plaintext)):
+
+        print (AlphaMove(plaintext[i],alphabet.find(keyt[i%len(keyt)])),end="")
+    
+    print("")
+
+
+def main():
+
+    Scanf()
+    
+    EnCryption()
+
+
+
+if "__main__" == __name__:
+
+    main()
+
+
+
+
